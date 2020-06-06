b158c1c827ea134095a08066d45a4a5c3b558d5a
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-06-06 21:10:07 +0100

    solns to exercises

--- exercises/entourages_from_pseudometric.lean
+++ exercises/entourages_from_pseudometric.lean
@@ -21,38 +21,92 @@ variables {X : Type} (d : X → X → ℝ) [is_pseudometric d]
 def entourages :=
   {U : set (X × X) | ∃ ε > 0, ∀ x y : X, d x y ≤ ε → (x,y) ∈ U}
 
+-- this lemma is true by definition
+lemma mem_entourages (U : set (X × X)) :
+  U ∈ entourages d ↔ ∃ ε > 0, ∀ x y : X, d x y ≤ ε → (x,y) ∈ U := iff.rfl 
+
 -- The exerise is to show that the 5 axioms of a uniform space are
 -- satisfied.
 
+-- Hint: You can `rw mem_entourages` to change from `U ∈ entourages d` to the
+-- explicit epsilon definition.
+
+-- Hint: if `hU : U ∈ entourages d` then `obtain ⟨ε, hε, hεU⟩ := hU` will
+-- get you the ε which is a witness to U being an entourage for d.
+
+-- Axiom 1: the diagonal is in U
 lemma refl (U : set (X × X)) (hU : U ∈ entourages d) :
   ∀ (x : X), (x, x) ∈ U :=
 begin
-  sorry
+  obtain ⟨ε, hε, hεU⟩ := hU,
+  intro x,
+  apply hεU,
+  rw d_self d,
+  linarith
 end
 
+
+-- Axiom 2: anything bigger than an entourage is an entourage
 lemma bigger (U V : set (X × X)) (hU : U ∈ entourages d) (hUV : U ⊆ V) :
   V ∈ entourages d :=
 begin
-  sorry
+  obtain ⟨ε, hε, hεU⟩ := hU,
+  rw mem_entourages,
+  use ε,
+  use hε,
+  intros x y hxy,
+  apply hUV,
+  apply hεU,
+  assumption,
 end
 
+-- Axiom 3: Intersection of two entourages is an entourage
 lemma inter (U V : set (X × X)) (hU : U ∈ entourages d) (hV : V ∈ entourages d) :
   U ∩ V ∈ entourages d :=
 begin
-  sorry
+  rw mem_entourages at ⊢ hU hV,
+  obtain ⟨ε₁, hε₁, hε₁U⟩ := hU,
+  obtain ⟨ε₂, hε₂, hε₂V⟩ := hV,
+  use (min ε₁ ε₂),
+  split, apply lt_min; assumption,
+  intros x y hxy,
+  rw le_min_iff at hxy,
+  split,
+  { apply hε₁U,
+    exact hxy.1
+  },
+  { apply hε₂V,
+    exact hxy.2
+  }
 end
 
--- hint: this is a triangle inequality proof so ε/2 might be helpful
+-- Axiom 4: the "square root" axiom. 
+-- You'll need `mem_comp_ent` here (defined in the entourages file)
 lemma comp (U : set (X × X)) (hU : U ∈ entourages d) :
  ∃ (V : set (X × X)) (H : V ∈ entourages d), V ∘ V ⊆ U :=
 begin
-  sorry
+  obtain ⟨ε, hε, hεU⟩ := hU,
+  use {z : X × X | d z.1 z.2 ≤ ε/2},
+  split, use ε/2, split, linarith, intros, assumption,
+  rintro ⟨x,y⟩ h,
+  apply hεU,
+  rw mem_comp_ent at h,
+  rcases h with ⟨z, hxz, hzy⟩,
+  dsimp at *, 
+  calc d x y ≤ d x z + d z y : d_triangle d x z y
+    ...      ≤ ε : by linarith [hxz, hzy]
 end
 
+-- Axiom 5: the "transpose" axiom.
 lemma symm (U : set (X × X)) (hU : U ∈ entourages d) :
   {z : X × X | (z.snd, z.fst) ∈ U} ∈ entourages d :=
 begin
-  sorry
+  obtain ⟨ε, hε, hεU⟩ := hU,
+  use [ε, hε],
+  intros x y hxy,
+  apply hεU,
+  convert hxy using 1,
+  exact d_comm d y x
 end
  
 definition to_entourages : uniform_space_entourage X :=
