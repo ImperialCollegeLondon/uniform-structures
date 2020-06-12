bc1edd69c111222b096e2e9a00075ab148b6d37d
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-06-12 20:22:44 +0100

    finished

--- exercises/covers_from_pseudometric.lean
+++ exercises/covers_from_pseudometric.lean
@@ -11,6 +11,8 @@ import pseudometric_space
 -- This is a question about pseudometrics so let's put it
 -- in the pseudometric namespace
 
+open set
+
 namespace pseudometric
 
 -- Let X be a set (or a type), and let d be a pseudometric on X
@@ -28,14 +30,26 @@ iff.rfl -- true by definition
 -- Here's an obvious lemma: if 0 ≤ ε then x is in the closed ball centre x
 -- and radius ε
 
--- But do we need it?
+lemma self_mem_ball (x : X) (ε : ℝ) (hε : 0 ≤ ε) : x ∈ closed_ball d x ε :=
+begin
+  rw mem_closed_ball,
+  rw d_self d,
+  assumption
+end
 
--- lemma self_mem_ball (x : X) (ε : ℝ) (hε : 0 ≤ ε) : x ∈ closed_ball d x ε :=
--- begin
---   rw mem_closed_ball,
---   rw d_self d,
---   assumption
--- end
+-- It's useful to know that the collection of closed balls radius ε
+-- is a cover. 
+
+def closed_ball_cover (ε : ℝ) (hε : 0 ≤ ε) : cover X :=
+{ C := {C : set X | ∃ x : X, C = closed_ball d x ε},
+  cov := begin
+    intro x,
+    use closed_ball d x ε,
+    split,
+    { use x},
+    { apply self_mem_ball d x ε hε,}
+  end
+}
 
 -- Define Θ to be the set of covers of X with the following property:
 -- there exists ε ≥ 0 such that each set in the cover contains a closed ball
@@ -59,7 +73,7 @@ begin
   use 1,
   split, linarith,
   intro x,
-  use set.univ,
+  use univ,
   simp [univ_cover],
 end
 
@@ -77,8 +91,8 @@ begin
   rcases hPQ with ⟨W, hW, hVW⟩,
   use W,
   use hW,
-  refine set.subset.trans hεV (hVW V hV _),
-  rw set.ne_empty_iff_nonempty,
+  refine subset.trans hεV (hVW V hV _),
+  rw ne_empty_iff_nonempty,
   use x,
   simp,
   apply hεV,
@@ -92,7 +106,65 @@ lemma ub_mem (P Q : cover X) (hP : P ∈ Θ d) (hQ : Q ∈ Θ d) :
   ∃ R : cover X, R ∈ Θ d ∧ R <* P ∧ R <* Q :=
 begin
   rw mem_Θ at hP hQ,
-  sorry
+  rcases hP with ⟨εP, hεP, hP⟩,
+  rcases hQ with ⟨εQ, hεQ, hQ⟩,
+  have hε : 0 < min εP εQ,
+    apply lt_min; linarith,
+  set ε := min εP εQ / 3 with hε,
+  use closed_ball_cover d ε (by linarith),
+  split,
+  { rw mem_Θ,
+    use ε,
+    split, linarith,
+    intro x,
+    use closed_ball d x ε,
+    split, swap, refl,
+    simp [closed_ball_cover],
+    use x,
+  },
+  split,
+  { rw star_ref_iff,
+    rintros _ ⟨x, rfl⟩,
+    rcases hP x with ⟨U, hUP, hUε⟩, -- <- changes to rcases hQ x
+    use [U, hUP],
+    rintros _ ⟨y, rfl⟩ hxy,
+    rw ne_empty_iff_nonempty at hxy,
+    rcases hxy with ⟨z, hzx, hzy⟩,
+    refine subset.trans _ hUε,
+    intros t hty,
+    rw mem_closed_ball at ⊢ hzx hzy hty,
+    suffices : d x t ≤ 3 * ε,
+    { refine le_trans this _,
+      rw hε,
+      convert min_le_left εP εQ, -- <- changes to min_le_right
+      linarith,
+    },
+    calc d x t ≤ d x z + d z t : d_triangle d x z t
+    ...        ≤ d x z + (d z y + d y t) : by linarith [d_triangle d z y t]
+    ...        = d x z + (d y z + d y t) : by rw d_comm d z y
+    ...        ≤ 3 * ε : by linarith
+  },
+  { rw star_ref_iff,
+    rintros _ ⟨x, rfl⟩,
+    rcases hQ x with ⟨U, hUQ, hUε⟩,
+    use [U, hUQ],
+    rintros _ ⟨y, rfl⟩ hxy,
+    rw ne_empty_iff_nonempty at hxy,
+    rcases hxy with ⟨z, hzx, hzy⟩,
+    refine subset.trans _ hUε,
+    intros t hty,
+    rw mem_closed_ball at ⊢ hzx hzy hty,
+    suffices : d x t ≤ 3 * ε,
+    { refine le_trans this _,
+      rw hε,
+      convert min_le_right εP εQ,
+      linarith,
+    },
+    calc d x t ≤ d x z + d z t : d_triangle d x z t
+    ...        ≤ d x z + (d z y + d y t) : by linarith [d_triangle d z y t]
+    ...        = d x z + (d y z + d y t) : by rw d_comm d z y
+    ...        ≤ 3 * ε : by linarith
+  },
 end
  
 definition to_cover : dist_covers X :=
